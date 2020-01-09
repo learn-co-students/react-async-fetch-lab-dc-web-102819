7b89a2f4b2c267e75028a077a992a0375d3b495d
mattbechtel1 <bechma02@gmail.com>
2020-01-09 13:57:02 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React, { Component } from "react";
 
 
 export default class App extends Component {
+
+    state = { something: []}
+
+    // grab(data) {
+    //     // return data.map(datum => datum.keys[0].map(key => key.toString()))
+    //     // return data.map(person => person.name)
+    // }
+
     render() {
-        return <div>My App</div>
+        // debugger
+        return <div>{this.state.something.map(person => person.name)}</div>
     }
 
     componentDidMount() {
         fetch('http://api.open-notify.org/astros.json')
+        .then(response => response.json())
+        .then(data => this.setState(
+            {something: data.people}))
     }
+ 
 }
