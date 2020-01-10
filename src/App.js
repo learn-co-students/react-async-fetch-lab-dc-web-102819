d83ff2403ea919d61a92353da0427cae8c8a6753
github username <github email address>
2020-01-10 09:52:21 -0500

    changes made

--- src/App.js
+++ src/App.js
@@ -28,19 +28,3 @@ class App extends React.Component {
 
 export default App 
 
-
-
-//   render() {
-//     return (
-//       <div>
-//         {this.state.peopleInSpace.map((person, id) => <h1 key={id}>{person.name}</h1>)}
-//       </div>
-//     )
-// //   }
-
-//   componentDidMount() {
-//     fetch('http://api.open-notify.org/astros.json')
-//       .then(response => response.json())
-//       .then(({people}) => this.setState({ peopleInSpace: people }))
-
-//   }
